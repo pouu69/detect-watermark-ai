2e4b5694c515855f4a867d81547999e4879a9070
Park KwanUng[ 박관웅 ] <kwanung.park@linecorp.com>
2025-07-11 21:13:31 +0900

    fix code

--- terraform/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "gpt-watermark-detector-terraform-state"
-    key            = "terraform.tfstate"
-    region         = "ap-northeast-2" # 서울 리전
-    encrypt        = true
-    dynamodb_table = "gpt-watermark-detector-terraform-locks"
-  }
-}
